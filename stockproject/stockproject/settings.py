ed6ca483403a2aac0a092b892fc1463ff61f6930
Krzysztof Garczewski <103775561+kgarczewski@users.noreply.github.com>
2022-11-22 20:43:33 +0100

    Update settings.py

--- stockproject/stockproject/settings.py
+++ stockproject/stockproject/settings.py
@@ -84,20 +84,12 @@ ASGI_APPLICATION = 'stockproject.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 DATABASES = {
     'default': {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'live_stock',
-        'USER': 'postgres',
-        'PASSWORD': 'coderslab',
     }
 }
 CHANNEL_LAYERS = {
